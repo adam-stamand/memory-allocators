50a521e685ae7e6afa80772833bc3ec5e8b6bf8f
St Amand <astamand@ndc.nasa.gov>
2019-05-21 10:54:50 -0700

    adding sphinx documentation with doxygen support

---
+++ src/helloworld.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+/*! \brief Brief description.
+ *         Brief description continued.
+ *
+ *  Detailed description starts here.
+ */
+
+int main(void){
+
+
+	std::cout << "HELLO WORLD" << std::endl;
+	return 0;
+
+}
