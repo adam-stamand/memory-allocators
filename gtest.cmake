9ac1eebe647b5bb42f7cbe117480ffa8aec93368
Adam St. Amand <adam.stamand@gmail.com>
2019-08-12 02:03:06 -0700

    updated travis to download and build GTest

--- gtest.cmake
+++ gtest.cmake
@@ -9,7 +9,7 @@ set(GOOGLETEST_SOURCES
     ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest-all.cc
     ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest_main.cc
     )
-
+message(" Google Sources: '${GOOGLETEST_SOURCES}'")
 foreach(_source ${GOOGLETEST_SOURCES})
     set_source_files_properties(${_source} PROPERTIES GENERATED 1)
 endforeach()
